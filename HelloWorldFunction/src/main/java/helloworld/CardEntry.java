334339a5b54fde55ffc6ccd907993bb88a2a3129
Jacob Haller-Roby <jroby@staywell.com>
2020-03-31 10:25:11 -0700

    hello world

---
+++ HelloWorldFunction/src/main/java/helloworld/CardEntry.java
@@ -0,0 +1,47 @@
+package helloworld;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "card_entry")
+public class CardEntry {
+
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    public CardEntry() {
+    }
+
+    public CardEntry(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CardEntry{" + "id=" + id + ", name=" + name + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ HelloWorldFunction/src/main/java/helloworld/HibernateUtil.java
@@ -0,0 +1,43 @@
+package helloworld;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (null != sessionFactory) return sessionFactory;
+
+        Configuration configuration = new Configuration();
+        String jdbcUrl = "jdbc:mysql://"
+                + System.getenv("RDS_HOSTNAME")
+                + "/"
+                + System.getenv("RDS_DB_NAME");
+        String username = System.getenv("RDS_USERNAME");
+        String password = System.getenv("RDS_PASSWORD");
+
+        System.out.println(username);
+        configuration.setProperty("hibernate.connection.url", jdbcUrl)
+                .setProperty("hibernate.connection.username", username)
+                .setProperty("hibernate.connection.password", password);
+
+        configuration.addAnnotatedClass(CardEntry.class);
+
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+
+        try {
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            System.err.println("Initial SessionFactory creation failed. " + e);
+            throw new ExceptionInInitializerError();
+        }
+        return sessionFactory;
+    }
+}
